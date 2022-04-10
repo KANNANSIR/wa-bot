b6e4e9ae3535e58c5dc3f452bc9f62f39ab42f6f
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2022-04-10 11:06:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const client = require('./lib/client')
 const { DATABASE } = require('./config')
 const start = async () => {
-  console.log('wa-bot')
+  console.log('kriz ser')
   await DATABASE.sync()
   console.log('DB syncing')
   await client.connect()
